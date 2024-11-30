eab0a28f3a5daab87446996da1de69e69eead148
vivekg1033 <viveksgupta1033@gmail.com>
2024-11-30 20:54:57 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reacct-app-hosting/', // Replace with your GitHub repository name
+  base: '/vite-deploy/', // Replace with your GitHub repository name
 });
